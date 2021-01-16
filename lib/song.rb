8d79fa46bf4e952ff6827685c98e0980759d98e6
Nathan Miller <natem31190@gmail.com>
2021-01-15 20:51:26 -0500

    commit tree 73bf20ee3c0cc624d1b6ae48bc2c516c375cf413

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,59 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = Song.new
+    song.save
+    song
+    
+  end
+
+  def self.new_by_name(song_name)
+    song = Song.new
+    song.name = song_name
+    song
+  end
+
+  def self.create_by_name(song_name)
+    song = self.new_by_name(song_name)
+    song.save
+    song
+  
+  end
+
+def self.find_by_name(song_name)
+    @@all.find {|song| song.name == song_name}
+
+end
+
+def self.find_or_create_by_name(song_name)
+    if self.find_by_name(song_name) == nil
+      self.create_by_name(song_name)
+    else
+      self.find_by_name(song_name)
+#self.find_by_name(song_name) || self.create_by_name(song.name)
+    end   
+  end
+
+  def self.alphabetical
+    self.all.sort_by{|song| song.name}
+end
+
+def self.new_from_filename(file_name)
+  file_name.slice!(".mp3")
+  attribute = file_name.split(" - ")
+  song = Song.new 
+  song.name = attribute[1]
+  song.artist_name = attribute[0]
+  song
+  end
+
+def self.create_from_filename(file_name)
+  song = self.new_from_filename(file_name)
+  song.save
+end
+
+def self.destroy_all
+    @@all.clear
+end
 end
